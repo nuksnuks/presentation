131791e6878777ad3def0fffbd65bbd518685ffc
nuksnuks <davidwogelius@gmail.com>
2022-03-13 20:12:16 +0100

    onepager update

---
+++ main.js
@@ -0,0 +1,15 @@
+function front() {
+  document.getElementById("front").style.display = "block";
+  document.getElementById("cases").style.display = "none";
+  document.getElementById("aboutme").style.display = "none";
+}
+function cases() {
+  document.getElementById("front").style.display = "none";
+  document.getElementById("cases").style.display = "block";
+  document.getElementById("aboutme").style.display = "none";
+}
+function aboutme() {
+  document.getElementById("front").style.display = "none";
+  document.getElementById("cases").style.display = "none";
+  document.getElementById("aboutme").style.display = "block";
+}
